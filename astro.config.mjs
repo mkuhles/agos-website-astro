8f0ec4d71b22ccc42886a3f84afafb6aa1dd616b
Melanie Kuhles <melanie.kuhles@oder-spree-consulting.de>
2026-02-15 22:01:55 +0100

    hide missing content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,31 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    build: {
+      rollupOptions: {
+        onwarn(warning, warn) {
+          if (
+            warning?.code === 'UNUSED_EXTERNAL_IMPORT' &&
+            warning?.exporter === '@astrojs/internal-helpers/remote' &&
+            Array.isArray(warning?.names) &&
+            warning.names.every((name) =>
+              ['matchHostname', 'matchPathname', 'matchPort', 'matchProtocol'].includes(name)
+            ) &&
+            Array.isArray(warning?.ids) &&
+            warning.ids.some(
+              (id) =>
+                id?.includes('node_modules/astro/dist/assets/utils/remotePattern.js') ||
+                id?.includes('node_modules/astro/dist/assets/services/service.js')
+            )
+          ) {
+            return;
+          }
+
+          warn(warning);
+        }
+      }
+    }
+  }
+});
